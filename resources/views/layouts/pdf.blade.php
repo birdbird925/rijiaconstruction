0f961482cf86da4d94a3709a9e38370abc6eaa46
xiangwen94 <xiangwen94@gmail.com>
2017-06-05 20:32:52 +0800

    debug 1.7

--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -11,25 +11,25 @@
         font-family: 'msyh';
         font-style: normal;
         font-weight: normal;
-        src: url(http://188.166.215.33/fonts/msyh.ttf) format('truetype');
+        src: url(/fonts/msyh.ttf) format('truetype');
     }
     @font-face {
         font-family: 'Open Sans';
         font-style: normal;
         font-weight: normal;
-        src: url(http://188.166.215.33/fonts/OpenSans-Regular.ttf) format('truetype');
+        src: url(/fonts/OpenSans-Regular.ttf) format('truetype');
     }
     @font-face {
         font-family: 'Open Sans Bold';
         font-style: normal;
         font-weight: bold;
-        src: url(http://188.166.215.33/fonts/OpenSans-Bold.ttf) format('truetype');
+        src: url(/fonts/OpenSans-Bold.ttf) format('truetype');
     }
     @font-face {
         font-family: 'Open Sans ExtralBold';
         font-style: normal;
         font-weight: 800;
-        src: url(http://188.166.215.33/fonts/OpenSans-ExtraBold.ttf) format('truetype');
+        src: url(/fonts/OpenSans-ExtraBold.ttf) format('truetype');
     }
 
     body {
