798a8d2b48f9615845d6d9c9ac64ddec6657a5af
xiangwen94 <xiangwen94@gmail.com>
2017-06-08 22:39:00 +0800

    new update

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,15 @@ Route::get('/', function () {
 });
 
 // Auth routes
-Route::auth();
-// Route::get('register', function(){abort(404);})->name('register');
-// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-// Route::post('login', 'Auth\LoginController@login');
-// Route::get('logout', 'Auth\LoginController@logout');
-// Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-// Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-// Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-// Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+// Route::auth();
+Route::get('register', function(){abort(404);})->name('register');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::get('logout', 'Auth\LoginController@logout');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 
 // Admin
 Route::group(['prefix' => 'admin'], function () {
